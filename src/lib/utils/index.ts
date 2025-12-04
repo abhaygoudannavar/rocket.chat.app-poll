2f7c10d553f8cf256dc31537c67a0cdad82c3a4d
suhaan-24 <suhaanrk73@gmail.com>
2025-12-05 02:10:14 +0530

    feat(utils): add utility functions for time and ephemeral messages

---
+++ src/lib/utils/index.ts
@@ -0,0 +1,14 @@
+export { uuid } from './uuid';
+export {
+    calculateExpiryTime,
+    isExpired,
+    getTimeRemaining,
+    formatTimeRemaining,
+    formatTimestamp,
+    formatDuration
+} from './time';
+export {
+    sendEphemeralError,
+    sendEphemeralSuccess,
+    sendEphemeralInfo
+} from './ephemeral';
